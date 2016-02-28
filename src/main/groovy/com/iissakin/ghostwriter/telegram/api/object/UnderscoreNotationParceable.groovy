4539f084cbd52e92a069478d6ca8349905caa78d
Ilya Issakin <ilya.issakin@gmail.com>
2016-02-28 17:57:49 +0600

    fixed wrongly working propertyMissing() for telegram core objects

--- src/main/groovy/com/iissakin/ghostwriter/telegram/api/object/UnderscoreNotationParceable.groovy
+++ src/main/groovy/com/iissakin/ghostwriter/telegram/api/object/UnderscoreNotationParceable.groovy
@@ -5,6 +5,10 @@ package com.iissakin.ghostwriter.telegram.api.object
  * Date: 28.02.2016.
  */
 abstract class UnderscoreNotationParceable {
+
+    /*
+    Had to manually make transformations like "message_id" -> "messageId" due to the way objects are parsed from JSON.
+     */
     def propertyMissing(String name, Object value) {
         while (name.indexOf("_") != -1) {
             StringBuilder sb = new StringBuilder(name)
@@ -12,8 +16,8 @@ abstract class UnderscoreNotationParceable {
             sb.deleteCharAt(name.indexOf("_"))
 
             name = sb.toString()
-
-            this[name] = value
         }
+
+        if (this.hasProperty(name)) this[name] = value
     }
 }
